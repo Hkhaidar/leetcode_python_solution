364102a85ee2e5555a9403bab092a031d3c7911d
Haidar Ansari <38322871+Hkhaidar@users.noreply.github.com>
2021-07-04 10:46:15 +0530

    Add files via upload

---
+++ addtwono.py
@@ -0,0 +1,34 @@
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        a=l1
+        b=l2
+        arr1=[]
+        arr2=[]
+        while a:
+            arr1.append(a.val)
+            a=a.next
+               
+        while b:
+            arr2.append(b.val)
+            b=b.next
+        
+        
+        
+        arr1.reverse()
+        arr2.reverse()
+        
+        inta=int("".join(str(x) for x in arr1))
+        intb=int("".join(str(x) for x in arr2))
+        
+        c=list(str(inta+intb))
+        result=l3=ListNode(c.pop())
+        c.reverse()
+        
+        
+        for i in c:
+            l3.next=ListNode(i)
+            l3=l3.next
+        return result
+       
